5be79ed51fbcebf9fd593adb326e7c01eea2251d
ahmad.mardiana <ahmad.mardiana@ifg-life.id>
2023-03-09 08:48:18 +0700

    update about

--- pages/about.js
+++ pages/about.js
@@ -14,7 +14,43 @@ export default function Home({ allPostsData }) {
       <section className={utilStyles.headingMd}>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-      <ol class="relative border-l border-gray-200 dark:border-gray-700">                  
+      <ol class="relative border-l border-gray-200 dark:border-gray-700">
+            <li class="mb-10 ml-4">
+                <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+                <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">September 2022 - now</time>
+                <h3 class="text-lg font-semibold ">Java Developer At PT. IFG Life </h3>
+                <p class="text-base font-normal text-gray-500 dark:text-gray-400">Get started with dozens of web components and interactive elements built on top of Tailwind CSS.</p>
+            </li> 
+            <li class="mb-10 ml-4">
+                <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+                <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">September 2022 - Desember 2022</time>
+                <h3 class="text-lg font-semibold ">Freelance Node Js Developer At PT. Lestari Elektronik Otomasi </h3>
+                <p class="text-base font-normal text-gray-500 dark:text-gray-400">Get started with dozens of web components and interactive elements built on top of Tailwind CSS.</p>
+            </li> 
+            <li class="mb-10 ml-4">
+                <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+                <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">Januari 2022 - September 2022</time>
+                <h3 class="text-lg font-semibold ">Java Developer At PT. Fastana Logistik Indonesia</h3>
+                <p class="text-base font-normal text-gray-500 dark:text-gray-400">Get started with dozens of web components and interactive elements built on top of Tailwind CSS.</p>
+            </li> 
+            <li class="mb-10 ml-4">
+                <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+                <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">May 2021 - Desember 2021</time>
+                <h3 class="text-lg font-semibold ">Java Developer At PT. Mcash Integrasi</h3>
+                <p class="text-base font-normal text-gray-500 dark:text-gray-400">Get started with dozens of web components and interactive elements built on top of Tailwind CSS.</p>
+            </li>                
+            <li class="mb-10 ml-4">
+                <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+                <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">Februari 2020 - May 2020</time>
+                <h3 class="text-lg font-semibold ">Fullstack Developer at PT. Code Development Indonesia</h3>
+                <p class="text-base font-normal text-gray-500 dark:text-gray-400">3 Month Bootcamp Intens learning of Node Js and React Js and on the last month we created an E-Commerce Application as the last project</p>
+            </li>
+            <li class="mb-10 ml-4">
+                <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+                <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">September 2019 - Januari 2021</time>
+                <h3 class="text-lg font-semibold ">Network Operation Center Engineer at PT. Lima Menara Bintang</h3>
+                <p class="text-base font-normal text-gray-500 dark:text-gray-400"></p>
+            </li>
             <li class="mb-10 ml-4">
                 <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
                 <time class="mb-1 text-sm font-normal leading-none text-gray-500">September 2019</time>
@@ -22,18 +58,6 @@ export default function Home({ allPostsData }) {
                 <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">Finished Telecommunication Diploma Degree with IOT Project titled 
                 "Realization of integration of monitoring system and online gas sales using web application and android internet of things based"
                  </p>
-            </li>
-            <li class="mb-10 ml-4">
-                <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-                <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">September 2019 - Januari 2021</time>
-                <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Network Operation Center Engineer at PT. Lima Menara Bintang</h3>
-                <p class="text-base font-normal text-gray-500 dark:text-gray-400">All of the pages and components are first designed in Figma and we keep a parity between the two versions even as we update the project.</p>
-            </li>
-            <li class="ml-4">
-                <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-                <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">Februari 2021 - May 2022</time>
-                <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Node Js Fullstack Developer Bootcamp</h3>
-                <p class="text-base font-normal text-gray-500 dark:text-gray-400">Get started with dozens of web components and interactive elements built on top of Tailwind CSS.</p>
             </li>
         </ol>
       </section>
